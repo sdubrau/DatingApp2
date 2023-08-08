80e4887add27673fe15984390436f25ce4b33a2c
sdubrau <sdubrau@gmail.com>
2023-08-08 18:16:54 +0200

    End of section 13

--- client/src/app/_guards/prevent-unsaved-changes.guard.ts
+++ client/src/app/_guards/prevent-unsaved-changes.guard.ts
@@ -1,16 +1,11 @@
-import { Injectable } from '@angular/core';
-
+import { CanDeactivateFn } from '@angular/router';
 import { MemberEditComponent } from '../members/member-edit/member-edit.component';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class PreventUnsavedChangesGuard  {
-  canDeactivate(
-    component: MemberEditComponent): boolean {
+export const preventUnsavedChangesGuard: CanDeactivateFn<MemberEditComponent> = (component) => {
     if(component.editForm?.dirty){
       return confirm('Are you sure you want to continue? Any unsaved changes will be lost');
     }
+    
     return true;
-  }
+  
 }
--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
 import { TabsModule } from 'ngx-bootstrap/tabs';
+import { TimeagoModule } from 'ngx-timeago';
 import { Member } from 'src/app/_models/member';
 import { MembersService } from 'src/app/_services/members.service';
 
@@ -10,10 +12,11 @@ import { MembersService } from 'src/app/_services/members.service';
   standalone: true,
   templateUrl: './member-detail.component.html',
   styleUrls: ['./member-detail.component.css'],
-  imports:[CommonModule, TabsModule]
+  imports:[CommonModule, TabsModule, GalleryModule, TimeagoModule]
 })
 export class MemberDetailComponent implements OnInit {
   member: Member | undefined;
+  images: GalleryItem[] = [];
 
   constructor(private memberService: MembersService, private route: ActivatedRoute) {
 
@@ -24,27 +27,35 @@ export class MemberDetailComponent implements OnInit {
 
 
   }
-  getImages() {
-    if (!this.member) return [];
-    const imageUrls = [];
-    for (const photo of this.member.photos) {
-      imageUrls.push({
-        small: photo.url,
-        medium: photo.url,
-        big: photo.url,
-      })
-    }
-    return imageUrls;
-  }
+  // getImages() {
+  //   if (!this.member) return [];
+  //   const imageUrls = [];
+  //   for (const photo of this.member.photos) {
+  //     imageUrls.push({
+  //       small: photo.url,
+  //       medium: photo.url,
+  //       big: photo.url,
+  //     })
+  //   }
+  //   return imageUrls;
+  // }
   loadMember() {
     const username = this.route.snapshot.paramMap.get('username');
     if (!username) return;
     this.memberService.getMember(username).subscribe({
       next: member => {
-        this.member = member;
+        this.member = member,
+        this.getImages()
         
       }
     })
+  }
+
+  getImages(){
+    if(!this.member) return;
+    for(const photo of this.member?.photos){
+      this.images.push(new ImageItem({src: photo.url, thumb: photo.url}))
+    }
   }
 
 }
